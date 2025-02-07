d9f90ca8fe8bf881f465fbfa40d89179f5658c89
oliviertech212 <oliviertech212@gmail.com>
2025-02-07 08:31:49 +0200

    make recommandation cart responsive implement stack on screen

---
+++ lib/screens/job-page.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:job_app/utils/colors.dart';
+import 'package:job_app/utils/text_styles.dart';
+
+class JobDetailPage extends StatelessWidget {
+  final String company;
+  final String location;
+  final String position;
+  final String salary;
+  final List<String> skills;
+  final List<String> descriptions;
+  final String logoUrl;
+
+  JobDetailPage({
+    required this.company,
+    required this.location,
+    required this.position,
+    required this.salary,
+    required this.skills,
+    required this.descriptions,
+    required this.logoUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      initialIndex: 0,
+      child: Scaffold(
+        backgroundColor: AppColors.backgroundColor,
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 230,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/teamgoogle.jpeg"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(height: 46.0),
+                          Text(position, style: Theme.of(context).textTheme.headlineLarge),
+                          SizedBox(height: 8.0),
+                          Text(location, style: AppTextStyles.bodyText2),
+                          SizedBox(height: 16.0),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Column(
+                      children: [
+                       const  TabBar(
+                          labelStyle: TextStyle(fontStyle: FontStyle.normal),
+                            physics: AlwaysScrollableScrollPhysics(), 
+                          isScrollable: false,
+                          tabs:  [
+                            Tab(text: 'Description'),
+                            Tab(text: 'Company'),
+                            Tab(text: 'Applicant'),
+                            Tab(text: 'Salary'),
+                          ],
+                        ),
+                        IntrinsicHeight(
+                          child: SizedBox(
+                            height: 300,
+                            child: TabBarView(
+                              children: [
+                                SingleChildScrollView(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(16.0),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Job Responsibilities:",
+                                          style: Theme.of(context).textTheme.headlineSmall,
+                                        ),
+                                        ...descriptions.map((desc) => ListTile(
+                                          leading: Icon(Icons.circle, size: 8.0),
+                                          title: Text(desc, style: AppTextStyles.bodyText2),
+                                        )).toList(),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Center(
+                                  child: Text(
+                                    "Company Information",
+                                    style: AppTextStyles.bodyText1,
+                                  ),
+                                ),
+                                Center(
+                                  child: Text(
+                                    "Applicant Information",
+                                    style: AppTextStyles.bodyText1,
+                                  ),
+                                ),
+                                Center(
+                                  child: Text(
+                                    "Salary Information",
+                                    style: AppTextStyles.bodyText1,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 16.0),
+                        Text(
+                          "Skills Needed:",
+                          style: Theme.of(context).textTheme.headlineSmall,
+                        ),
+                        SizedBox(height: 8.0),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: skills
+                                .map((skill) => Text(
+                                      ".${skill}    ",
+                                      style: AppTextStyles.bodyText2,
+                                    ))
+                                .toList(),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              top: 180,
+              left: MediaQuery.of(context).size.width / 2 - 50,
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage(logoUrl),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/recommendation-card.dart
+++ lib/widgets/recommendation-card.dart
@@ -1,68 +1,106 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:job_app/screens/job-page.dart';
 import 'package:job_app/utils/colors.dart';
 import 'package:job_app/utils/text_styles.dart';
 
-Widget recommendationCard( BuildContext context,
-      String company, String location, String position, String details, String salary , String? imageUrl , List<String> skills , List<String> descriptions) {
-    return Card(
-      color: company !="Facebook"?  AppColors.lightBlue:Colors.white,
+Widget recommendationCard(
+  BuildContext context,
+  String company,
+  String location,
+  String position,
+  String details,
+  String salary,
+  String imageUrl,
+  List<String> skills,
+  List<String> descriptions,
+) {
+  return GestureDetector(
+    onTap: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => JobDetailPage(
+            company: company,
+            location: location,
+            position: position,
+            salary: salary,
+            skills: skills,
+            descriptions: descriptions,
+            logoUrl: imageUrl,
+          ),
+        ),
+      );
+    },
+    child: Card(
+      color: company != "Facebook" ? AppColors.lightBlue : Colors.white,
       elevation: 2.0,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(8.0),
       ),
-      child: SizedBox(
-        width: 300.0,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 6.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-             
-             ListTile(
-              contentPadding: EdgeInsets.zero,
-               leading: CircleAvatar(
-                 radius: 20.0,
-                 backgroundImage: Image.asset(imageUrl!).image,
-               ),
-               title: Text(company, style: Theme.of(context).textTheme.headlineSmall,),
-               subtitle: Text(location, style: AppTextStyles.bodyText2),
-               trailing: Icon(Icons.description_outlined),
-              ),
-              SizedBox(height: 8.0),
-              Text(position,style: Theme.of(context).textTheme.headlineSmall,),
-              SizedBox(height: 4.0),
-              Text(details, style: AppTextStyles.bodyText2),
-              SizedBox(height: 8.0),
-            
-              Row(
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          double screenWidth = MediaQuery.of(context).size.width;
+          double cardWidth = screenWidth < 500 ? 300 : 500;
+          return SizedBox(
+            width: cardWidth,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 6.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                 ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColors.primaryColor,
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
+                  ListTile(
+                    contentPadding: EdgeInsets.zero,
+                    leading: CircleAvatar(
+                      radius: 20.0,
+                      backgroundImage: Image.asset(imageUrl!).image,
+                    ),
+                    title: Text(
+                      company,
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    subtitle: Text(location, style: AppTextStyles.bodyText2),
+                    trailing: Icon(Icons.description_outlined),
                   ),
-                ),
-                child: Text("Apply Now"),
-              ),
-
-              Row(
-                children: [
-                 Text("${salary}",style: Theme.of(context).textTheme.headlineSmall,),
-                  Text("/Month", style: AppTextStyles.bodyText2),
+                  SizedBox(height: 8.0),
+                  Text(position, style: Theme.of(context).textTheme.headlineSmall),
+                  SizedBox(height: 4.0),
+                  Text(details, style: AppTextStyles.bodyText2),
+                  SizedBox(height: 8.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.primaryColor,
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        child: Text("Apply Now"),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "${salary}",
+                            style: Theme.of(context).textTheme.headlineSmall,
+                          ),
+                          Text("/Month", style: AppTextStyles.bodyText2),
+                        ],
+                      ),
+                    ],
+                  ),
                 ],
               ),
-                ])
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
-    );
-  }
+    ),
+  );
+}
